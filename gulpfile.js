dbe8e5ebfdeedf3d3bb83201eeb09e53bcc39f8a
Fabrice Flore-Thébault <ffloreth@redhat.com>
2021-09-28 16:08:46 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const connect = require('gulp-connect')
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const fs = require('fs')
+const generator = require('@antora/site-generator-default')
+const { reload: livereload } = process.env.LIVERELOAD === 'true' ? require('gulp-connect') : {}
+const { parallel, series, src, watch } = require('gulp')
+const yaml = require('js-yaml')
+
+const playbookFilename = 'antora-playbook-for-development.yml'
+const playbook = yaml.safeLoad(fs.readFileSync(playbookFilename, 'utf8'))
+const outputDir = (playbook.output || {}).dir || './build/site'
+const serverConfig = { name: 'Preview Site', livereload, host: '0.0.0.0', port: 4000, root: outputDir }
+const antoraArgs = ['--playbook', playbookFilename]
+const watchPatterns = playbook.content.sources.filter((source) => !source.url.includes(':')).reduce((accum, source) => {
+    accum.push(`./antora.yml`)
+    accum.push(`./modules/**/*`)
+    return accum
+}, [])
+
+function generate(done) {
+    generator(antoraArgs, process.env)
+        .then(() => done())
+        .catch((err) => {
+            console.log(err)
+            done()
+        })
+}
+
+async function serve(done) {
+    connect.server(serverConfig, function () {
+        this.server.on('close', done)
+        watch(watchPatterns, series(generate, testhtml))
+        if (livereload) watch(this.root).on('change', (filepath) => src(filepath, { read: false }).pipe(livereload()))
+    })
+}
+
+
+async function testhtml() {
+    // Report links errors but don't make gulp fail.
+    try {
+        const { stdout, stderr } = await exec('htmltest')
+        console.log(stdout);
+        console.error(stderr);
+    }
+    catch (error) {
+        console.log(error.stdout);
+        console.log(error.stderr);
+        return;
+    }
+}
+
+
+exports.default = series(
+    generate,
+    serve,
+    parallel(testhtml)
+);
